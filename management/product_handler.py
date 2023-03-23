fb4e7e38ab452cda56131d45872b1b38f9f4cf73
Caio Alberto <caioalberto-@hotmail.com>
2023-03-23 15:38:49 -0300

    feat: tarefa 2 finalizada

--- management/product_handler.py
+++ management/product_handler.py
@@ -3,7 +3,7 @@ from menu import products
 
 def get_product_by_id(id: int):
     for index, dictionary in enumerate(products):
-        if (dictionary["_id"] == id):
+        if dictionary["_id"] == id:
             return products[index]
 
     return {}
@@ -13,6 +13,21 @@ def get_products_by_type(type: str):
     new_list = []
 
     for index, dictionary in enumerate(products):
-        if (dictionary["type"] == type):
+        if dictionary["type"] == type:
             new_list.append(products[index])
     return new_list
+
+
+def add_product(menu: list, **kwargs):
+    index_list = []
+    id = 1
+    if len(menu) > 0:
+        for product in menu:
+            index_list.append(product["_id"])
+        index_list.sort()
+        id = index_list[-1] + 1
+
+    kwargs["_id"] = id
+    menu.append(kwargs)
+
+    return kwargs
